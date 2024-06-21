037316a634a99b4dfbfbade5348e202a89dd3935
Iqbal mustakim <159795925+Iqbalyo@users.noreply.github.com>
2024-06-21 19:16:53 +0700

    Add files via upload

---
+++ src/scripts/utils/like-button-initiator.js
@@ -0,0 +1,47 @@
+import { createLikeButtonTemplate, createLikedButtonTemplate } from '../views/templat/template-creator';
+import FavoriteRestaurantIdb from '../data/favorite-restorant-idb';
+
+const LikeButtonInitiator = {
+  async init({ likeButtonContainer, restaurant }) {
+    this._likeButtonContainer = likeButtonContainer;
+    this._restaurant = restaurant;
+
+    await this._renderButton();
+  },
+
+  async _renderButton() {
+    const { id } = this._restaurant; // Pastikan menggunakan this._restaurant
+    if (await this._isRestaurantExist(id)) {
+      this._renderLiked();
+    } else {
+      this._renderLike();
+    }
+  },
+
+  async _isRestaurantExist(id) {
+    const restaurant = await FavoriteRestaurantIdb.getRestaurant(id);
+    return !!restaurant;
+  },
+
+  _renderLike() {
+    this._likeButtonContainer.innerHTML = createLikeButtonTemplate();
+
+    const likeButton = document.querySelector('#likeButton');
+    likeButton.addEventListener('click', async () => {
+      await FavoriteRestaurantIdb.putRestaurant(this._restaurant);
+      await this._renderButton(); // Memastikan memanggil ulang _renderButton dengan async/await
+    });
+  },
+
+  _renderLiked() {
+    this._likeButtonContainer.innerHTML = createLikedButtonTemplate();
+
+    const likeButton = document.querySelector('#likeButton');
+    likeButton.addEventListener('click', async () => {
+      await FavoriteRestaurantIdb.deleteRestaurant(this._restaurant.id);
+      await this._renderButton(); // Memastikan memanggil ulang _renderButton dengan async/await
+    });
+  },
+};
+
+export default LikeButtonInitiator;
